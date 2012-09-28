7583b4643e217485afac03394cb356c7a466903a
Mattias Rauter <mattias.rauter@student.tugraz.at>
2012-09-28 23:40:13 +0200

    black frame (with HelperSprite)

---
+++ Catty/HelperSprite.h
@@ -0,0 +1,13 @@
+//
+//  HelperSprite.h
+//  Catty
+//
+//  Created by Mattias Rauter on 28.09.12.
+//  Copyright (c) 2012 Graz University of Technology. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HelperSprite : NSObject
+
+@end
---
+++ Catty/HelperSprite.m
@@ -0,0 +1,13 @@
+//
+//  HelperSprite.m
+//  Catty
+//
+//  Created by Mattias Rauter on 28.09.12.
+//  Copyright (c) 2012 Graz University of Technology. All rights reserved.
+//
+
+#import "HelperSprite.h"
+
+@implementation HelperSprite
+
+@end
