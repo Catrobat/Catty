0e6af235b98780442eb8fd515168be9395b436a0
Christof Stromberger <stromberger@student.tugraz.at>
2012-09-28 18:40:22 +0200

    Merged into master

--- Catty/CattyAppDelegate.m
+++ Catty/CattyAppDelegate.m
@@ -5,7 +5,7 @@
 //  Created by Christof Stromberger on 07.07.12.
 //  Copyright (c) 2012 Graz University of Technology. All rights reserved.
 //
-
+ 
 #import "CattyAppDelegate.h"
 
 @implementation CattyAppDelegate
