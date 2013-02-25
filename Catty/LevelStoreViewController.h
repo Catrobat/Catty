e7b561b8c36ac688cbe34a03efce75395a601512
Christof Stromberger <stromberger@student.tugraz.at>
2013-02-25 16:08:04 +0100

    Minor changes

---
+++ Catty/LevelStoreViewController.h
@@ -0,0 +1,13 @@
+//
+//  LevelStoreViewController.h
+//  Catty
+//
+//  Created by Christof Stromberger on 25.02.13.
+//  Copyright (c) 2013 Graz University of Technology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LevelStoreViewController : UITableViewController
+
+@end
