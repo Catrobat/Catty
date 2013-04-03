e2df666e8c97a34ef7ee335e3728536c421f1ebd
Christof Stromberger <stromberger@student.tugraz.at>
2013-04-03 18:21:08 +0200

    Added sample code

--- src/Classes/Game.m
+++ src/Classes/Game.m
@@ -13,6 +13,8 @@
 - (void)onImageTouched:(SPTouchEvent *)event;
 - (void)onResize:(SPResizeEvent *)event;
 
+@property (nonatomic, strong) SPImage *image; // just temp
+
 @end
 
 
@@ -81,18 +83,20 @@
     image.y = background.height / 2 + 40;
     [_contents addChild:image];
     
+    self.image = image;
+    
     [self updateLocations];
     
     // play a sound when the image is touched
     [image addEventListener:@selector(onImageTouched:) atObject:self forType:SP_EVENT_TYPE_TOUCH];
     
     // and animate it a little
-    SPTween *tween = [SPTween tweenWithTarget:image time:1.5 transition:SP_TRANSITION_EASE_IN_OUT];
+    /*SPTween *tween = [SPTween tweenWithTarget:image time:1.5 transition:SP_TRANSITION_EASE_IN_OUT];
     [tween animateProperty:@"y" targetValue:image.y + 30];
     [tween animateProperty:@"rotation" targetValue:0.1];
     tween.repeatCount = 0; // repeat indefinitely
     tween.reverse = YES;
-    [Sparrow.juggler addObject:tween];
+    [Sparrow.juggler addObject:tween];*/
     
 
     // The controller autorotates the game to all supported device orientations. 
@@ -103,7 +107,7 @@
     // To force the game to start up in landscape, add the key "Initial Interface Orientation"
     // to the "App-Info.plist" file and choose any landscape orientation.
     
-    [self addEventListener:@selector(onResize:) atObject:self forType:SP_EVENT_TYPE_RESIZE];
+    //[self addEventListener:@selector(onResize:) atObject:self forType:SP_EVENT_TYPE_RESIZE];
     
     // Per default, this project compiles as a universal application. To change that, enter the 
     // project info screen, and in the "Build"-tab, find the setting "Targeted device family".
@@ -127,16 +131,71 @@
 
 - (void)onImageTouched:(SPTouchEvent *)event
 {
-    NSSet *touches = [event touchesWithTarget:self andPhase:SPTouchPhaseEnded];
-    if ([touches anyObject]) [Media playSound:@"sound.caf"];
+    // TEST for resize
+//    SPTween *tween = [SPTween tweenWithTarget:self.image time:1.0f];
+//    [tween animateProperty:@"scaleX" targetValue:self.image.scaleX * 1.5];
+//    [tween animateProperty:@"scaleY" targetValue:self.image.scaleY * 1.5];
+//    tween.repeatCount = 1;
+//    [Sparrow.juggler addObject:tween];
+
+    // TEST for rotation
+//    SPTween *tween = [SPTween tweenWithTarget:self.image time:1.0f];
+//    [tween animateProperty:@"rotation" targetValue:self.image.rotation + 3.6f];
+//    tween.repeatCount = 1;
+//    [Sparrow.juggler addObject:tween];
+    
+    // TEST for move
+//    SPTween *tween = [SPTween tweenWithTarget:self.image time:1.0f];
+//    [tween animateProperty:@"x" targetValue:self.image.x + 20.0f];
+//    [tween animateProperty:@"y" targetValue:self.image.y - 50.0f];
+//    tween.repeatCount = 1;
+//    [Sparrow.juggler addObject:tween];
+    
+    // TEST for position
+//    SPTween *tween = [SPTween tweenWithTarget:self.image time:1.0f];
+//    [tween animateProperty:@"x" targetValue:10];
+//    [tween animateProperty:@"y" targetValue:10];
+//    tween.repeatCount = 1;
+//    [Sparrow.juggler addObject:tween];
+    
+    // TEST for alpha
+//    SPTween *tween = [SPTween tweenWithTarget:self.image time:1.0f];
+//    [tween animateProperty:@"alpha" targetValue:0.2f];
+//    tween.repeatCount = 1;
+//    [Sparrow.juggler addObject:tween];
+    
+    // TEST for brightness (<100%)
+    SPImage *image = (SPImage*)self.image;
+    //image.color = SP_COLOR(100, 200, 255);
+
+    CGFloat scale = 0.5f; // MUST be <1.0 (=100%)
+
+    uint hexColor = image.color;
+    uint r = (hexColor >> 16);
+    uint g = ((hexColor << 16) >> 24);
+    uint b = ((hexColor << 24) >> 24);
+    NSLog(@"Color in hex: %x", hexColor);
+    NSLog(@"R: %d", r);
+    NSLog(@"G: %d", g);
+    NSLog(@"B: %d", b);
+
+    // set color
+    image.color = SP_COLOR(r*scale, g*scale, b*scale);
+    NSLog(@"COLOR SET");
+    
+    
+    
+    
+    /*NSSet *touches = [event touchesWithTarget:self andPhase:SPTouchPhaseEnded];
+    if ([touches anyObject]) [Media playSound:@"sound.caf"];*/
 }
 
-- (void)onResize:(SPResizeEvent *)event
-{
-    NSLog(@"new size: %.0fx%.0f (%@)", event.width, event.height, 
-          event.isPortrait ? @"portrait" : @"landscape");
-    
-    [self updateLocations];
-}
+//- (void)onResize:(SPResizeEvent *)event
+//{
+//    NSLog(@"new size: %.0fx%.0f (%@)", event.width, event.height, 
+//          event.isPortrait ? @"portrait" : @"landscape");
+//    
+//    [self updateLocations];
+//}
 
 @end
