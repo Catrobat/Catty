53665f38e1a63aa5372583b338864e6c621c9732
Dominik Ziegler <dominik.j.ziegler@gmail.com>
2013-05-07 19:46:44 +0200

    commit for christof

---
+++ src/Catty/XMLObjectReference.h
@@ -0,0 +1,13 @@
+//
+//  XMLObjectReference.h
+//  Catty
+//
+//  Created by Dominik Ziegler on 5/7/13.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XMLObjectReference : NSObject
+
+@end
---
+++ src/Catty/XMLObjectReference.m
@@ -0,0 +1,13 @@
+//
+//  XMLObjectReference.m
+//  Catty
+//
+//  Created by Dominik Ziegler on 5/7/13.
+//
+//
+
+#import "XMLObjectReference.h"
+
+@implementation XMLObjectReference
+
+@end
