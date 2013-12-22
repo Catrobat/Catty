8446ad3d324ce68692bd1c05762f3ba2fbab6be9
Ralph Samer <ralph.samer@student.tugraz.at>
2013-12-23 00:20:25 +0100

    adjust alignment of placeholders

--- src/Catty/BaseTableViewController.m
+++ src/Catty/BaseTableViewController.m
@@ -38,70 +38,77 @@
 #pragma marks init
 - (void)initPlaceHolder
 {
-  self.placeholder = [[UIView alloc] initWithFrame:self.tableView.bounds];
-  self.placeholder.tag = kPlaceHolderTag;
-
-  // setup title label
-  self.placeholderTitleLabel = [[UILabel alloc] init];
-  self.placeholderTitleLabel.textAlignment = NSTextAlignmentCenter;
-  self.placeholderTitleLabel.backgroundColor = [UIColor clearColor];
-  self.placeholderTitleLabel.textColor = [UIColor skyBlueColor];
-  self.placeholderTitleLabel.font = [self.placeholderTitleLabel.font fontWithSize:45];
-
-  // setup description label
-  self.placeholderDescriptionLabel = [[UILabel alloc] init];
-  self.placeholderDescriptionLabel.textAlignment = NSTextAlignmentCenter;
-  self.placeholderDescriptionLabel.backgroundColor = [UIColor clearColor];
-  self.placeholderDescriptionLabel.textColor = [UIColor skyBlueColor];
-  [self.placeholder addSubview:self.placeholderTitleLabel];
-  [self.placeholder addSubview:self.placeholderDescriptionLabel];
-  [self.tableView addSubview:self.placeholder];
-  self.tableView.alwaysBounceVertical = self.placeholder.hidden = YES;
+    self.placeholder = [[UIView alloc] initWithFrame:self.tableView.bounds];
+    self.placeholder.tag = kPlaceHolderTag;
+    
+    // setup title label
+    self.placeholderTitleLabel = [[UILabel alloc] init];
+    self.placeholderTitleLabel.textAlignment = NSTextAlignmentCenter;
+    self.placeholderTitleLabel.backgroundColor = [UIColor clearColor];
+    self.placeholderTitleLabel.textColor = [UIColor skyBlueColor];
+    self.placeholderTitleLabel.font = [self.placeholderTitleLabel.font fontWithSize:45];
+    
+    // setup description label
+    self.placeholderDescriptionLabel = [[UILabel alloc] init];
+    self.placeholderDescriptionLabel.textAlignment = NSTextAlignmentCenter;
+    self.placeholderDescriptionLabel.backgroundColor = [UIColor clearColor];
+    self.placeholderDescriptionLabel.textColor = [UIColor skyBlueColor];
+    [self.placeholder addSubview:self.placeholderTitleLabel];
+    [self.placeholder addSubview:self.placeholderDescriptionLabel];
+    [self.tableView addSubview:self.placeholder];
+    self.tableView.alwaysBounceVertical = self.placeholder.hidden = YES;
 }
 
 - (void)initTableView
 {
-  self.tableView.delegate = self;
-  self.tableView.dataSource = self;
-  [self.tableView setSeparatorStyle:UITableViewCellSeparatorStyleNone];
-  self.tableView.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"darkblue"]];
-  UITableViewHeaderFooterView *headerViewTemplate = [[UITableViewHeaderFooterView alloc] initWithReuseIdentifier:kTableHeaderIdentifier];
-  headerViewTemplate.contentView.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"darkblue"]];
-  [self.tableView addSubview:headerViewTemplate];
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self;
+    [self.tableView setSeparatorStyle:UITableViewCellSeparatorStyleNone];
+    self.tableView.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"darkblue"]];
+    UITableViewHeaderFooterView *headerViewTemplate = [[UITableViewHeaderFooterView alloc] initWithReuseIdentifier:kTableHeaderIdentifier];
+    headerViewTemplate.contentView.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"darkblue"]];
+    [self.tableView addSubview:headerViewTemplate];
 }
 
 #pragma mark - getters and setters
 - (void)setPlaceHolderTitle:(NSString*)title Description:(NSString*)description
 {
-  // title label
-  self.placeholderTitleLabel.text = title;
-  [self.placeholderTitleLabel sizeToFit];
-  CGRect frame = self.tableView.bounds;
-  CGRect bounds = self.placeholderTitleLabel.bounds;
-  #define placeholderTitlePaddingBottom 15.0f
-  frame.origin.y = (frame.size.height/2.0f)-bounds.size.height-placeholderTitlePaddingBottom;
-  frame.size.height = bounds.size.height;
-  self.placeholderTitleLabel.frame = frame;
+    // title label
+    self.placeholderTitleLabel.text = title;
+    [self.placeholderTitleLabel sizeToFit];
 
-  // description label
-  self.placeholderDescriptionLabel.text = description;
-  [self.placeholderDescriptionLabel sizeToFit];
-  bounds = self.placeholderDescriptionLabel.bounds;
-  frame = self.tableView.bounds;
-  frame.origin.y = (frame.size.height/2.0f);
-  frame.size.height = bounds.size.height;
-  self.placeholderDescriptionLabel.frame = frame;
+    // description label
+    self.placeholderDescriptionLabel.text = description;
+    [self.placeholderDescriptionLabel sizeToFit];
+
+    // set alignemnt: middle center
+    CGRect frameTitle = self.placeholderTitleLabel.frame;
+    CGRect frameDescription = self.placeholderDescriptionLabel.frame;
+    CGFloat totalHeight = frameTitle.size.height + frameDescription.size.height;
+    NSUInteger offsetY;
+
+    // this sets vertical alignment of the placeholder to the center of table view
+//    offsetY = (self.navigationController.toolbar.frame.origin.y - self.navigationController.navigationBar.frame.size.height - totalHeight)/2.0f;
+    // this sets vertical alignment of the placeholder to the center of whole screen
+    offsetY = (self.navigationController.toolbar.frame.origin.y - totalHeight)/2.0f - self.navigationController.navigationBar.frame.size.height;
+    frameTitle.origin.y = offsetY;
+    frameTitle.origin.x = frameDescription.origin.x = 0.0f;
+    frameTitle.size.width = frameDescription.size.width = self.view.frame.size.width;
+    self.placeholderTitleLabel.frame = frameTitle;
+
+    frameDescription.origin.y = offsetY + frameTitle.size.height;
+    self.placeholderDescriptionLabel.frame = frameDescription;
 }
 
 - (void)showPlaceHolder:(BOOL)show
 {
-  self.tableView.alwaysBounceVertical = self.placeholder.hidden = (! show);
+    self.tableView.alwaysBounceVertical = self.placeholder.hidden = (! show);
 }
 
 #pragma mark - Table view data source
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-  return [TableUtil getHeightForImageCell];
+    return [TableUtil getHeightForImageCell];
 }
 
 @end
