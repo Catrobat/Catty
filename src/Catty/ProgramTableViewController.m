96d6cab12ebdbf81a292652d367d4010acf598e3
Luca Franceschini <lfranceschini1@gmail.com>
2014-05-08 23:17:57 +0200

    cell background color fix

--- src/Catty/ProgramTableViewController.m
+++ src/Catty/ProgramTableViewController.m
@@ -260,6 +260,7 @@ UINavigationBarDelegate>
     SpriteObject *object = [self.program.objectList objectAtIndex:index];
     imageCell.iconImageView.image = nil;
     [imageCell.iconImageView setBorder:[UIColor skyBlueColor] Width:kDefaultImageCellBorderWidth];
+    imageCell.backgroundColor = UIColor.darkBlueColor;
     if (! [object.lookList count]) {
         imageCell.titleLabel.text = object.name;
         return imageCell;
