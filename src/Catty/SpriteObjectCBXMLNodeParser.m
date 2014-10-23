9359ab06d7a4992013d3001cbaaa9bb7150cdcd4
Ralph Samer <ralph.samer@student.tugraz.at>
2014-10-23 03:00:47 +0200

    new script node parser

--- src/Catty/SpriteObjectCBXMLNodeParser.m
+++ src/Catty/SpriteObjectCBXMLNodeParser.m
@@ -24,21 +24,20 @@
 #import "GDataXMLNode.h"
 #import "CBXMLValidator.h"
 #import "SpriteObject.h"
+#import "LookCBXMLNodeParser.h"
+#import "SoundCBXMLNodeParser.h"
+#import "ScriptCBXMLNodeParser.h"
 
 @implementation SpriteObjectCBXMLNodeParser
 
 - (id)parseFromElement:(GDataXMLElement*)xmlElement
 {
-    [CBXMLValidator exceptionIfNil:xmlElement message:@"Parsed an empty object entry!"];
-    [NSString stringWithFormat:@"test"];
-
+    [XMLError exceptionIfNode:xmlElement isNilOrNodeNameNotEquals:@"object"];
     NSArray *attributes = [xmlElement attributes];
-    if ([attributes count] != 1) {
-        [NSException raise:@"InvalidObjectException"
-                    format:@"Parsed name-attribute of object is invalid or empty!"];
-    }
+    [XMLError exceptionIf:[attributes count] notEquals:1
+                  message:@"Parsed name-attribute of object is invalid or empty!"];
 
-    SpriteObject *spriteObject = [[SpriteObject alloc] init];
+    SpriteObject *spriteObject = [SpriteObject new];
     GDataXMLNode *attribute = [attributes firstObject];
     GDataXMLElement *pointedObjectElement = nil;
     // check if normal or pointed object
@@ -47,105 +46,79 @@
     } else if ([attribute.name isEqualToString:@"reference"]) {
         NSString *xPath = [attribute stringValue];
         NSArray *queriedObjects = [xmlElement nodesForXPath:xPath error:nil];
-        if ([queriedObjects count] != 1) {
-            [NSException raise:@"InvalidObjectException"
-                        format:@"Invalid reference in object. No or too many pointed objects found!"];
-        }
+        [XMLError exceptionIf:[queriedObjects count] notEquals:1
+                      message:@"Invalid reference in object. No or too many pointed objects found!"];
         pointedObjectElement = [queriedObjects firstObject];
+        [XMLError exceptionIfNode:pointedObjectElement isNilOrNodeNameNotEquals:@"pointedObject"];
         GDataXMLNode *nameAttribute = [pointedObjectElement attributeForName:@"name"];
-        if (! nameAttribute) {
-            [NSException raise:@"InvalidObjectException"
-                        format:@"PointedObject must contain a name attribute"];
-        }
+        [XMLError exceptionIfNil:nameAttribute message:@"PointedObject must contain a name attribute"];
         spriteObject.name = [nameAttribute stringValue];
     } else {
-        [NSException raise:@"InvalidObjectException" format:@"Unsupported attribute: %@!", attribute.name];
+        [XMLError exceptionWithMessage:@"Unsupported attribute: %@!", attribute.name];
     }
     NSLog(@"<object name=\"%@\">", spriteObject.name);
 
-    // TODO: support for WEAK (!) properties required here!!
     spriteObject.lookList = [self parseAndCreateLooks:(pointedObjectElement ? pointedObjectElement : xmlElement)];
     spriteObject.soundList = [self parseAndCreateSounds:(pointedObjectElement ? pointedObjectElement : xmlElement)];
-    // TODO: implement this...
-    //        spriteObject.scriptList = [self parseAndCreateSounds:(pointedObjectElement ? pointedObjectElement : objectElement)];
+    spriteObject.scriptList = [self parseAndCreateScripts:(pointedObjectElement ? pointedObjectElement : xmlElement)];
     return spriteObject;
 }
 
 - (NSMutableArray*)parseAndCreateLooks:(GDataXMLElement*)objectElement
 {
     NSArray *lookListElements = [objectElement elementsForName:@"lookList"];
-    if ([lookListElements count] != 1) {
-        [NSException raise:@"InvalidObjectException" format:@"No lookList given!"];
-    }
-    
+    [XMLError exceptionIf:[lookListElements count] notEquals:1 message:@"No lookList given!"];
+
     NSArray *lookElements = [[lookListElements firstObject] children];
     if (! [lookElements count]) {
+        // TODO: ask team if we should return nil or an empty NSMutableArray in this case!!
         return nil;
     }
 
     NSMutableArray *lookList = [NSMutableArray arrayWithCapacity:[lookElements count]];
-//    for (GDataXMLElement *lookElement in lookElements) {
-//        Look *look = [[Look alloc] init];
-//        GDataXMLNode *nameAttribute = [lookElement attributeForName:@"name"];
-//        if (! nameAttribute) {
-//            [NSException raise:@"InvalidObjectException"
-//                        format:@"Look must contain a name attribute"];
-//        }
-//        look.name = [nameAttribute stringValue];
-//        NSArray *lookChildElements = [lookElement children];
-//        if ([lookChildElements count] != 1) {
-//            [NSException raise:@"InvalidObjectException"
-//                        format:@"Look must contain a filename child node"];
-//        }
-//        GDataXMLNode *fileNameElement = [lookChildElements firstObject];
-//        if (! [fileNameElement.name isEqualToString:@"fileName"]) {
-//            [NSException raise:@"InvalidObjectException"
-//                        format:@"Look contains wrong child node"];
-//        }
-//        look.fileName = [fileNameElement stringValue];
-//    }
+    LookCBXMLNodeParser *lookParser = [LookCBXMLNodeParser new];
+    for (GDataXMLElement *lookElement in lookElements) {
+        [lookList addObject:[lookParser parseFromElement:lookElement]];
+    }
     return lookList;
 }
 
 - (NSMutableArray*)parseAndCreateSounds:(GDataXMLElement*)objectElement
 {
     NSArray *soundListElements = [objectElement elementsForName:@"soundList"];
-    // TODO: increase readability, use macros for such sanity checks...
-    if ([soundListElements count] != 1) {
-        [NSException raise:@"InvalidObjectException" format:@"No soundList given!"];
-    }
+    [XMLError exceptionIf:[soundListElements count] notEquals:1 message:@"No soundList given!"];
 
     NSArray *soundElements = [[soundListElements firstObject] children];
     if (! [soundElements count]) {
+        // TODO: ask team if we should return nil or an empty NSMutableArray in this case!!
         return nil;
     }
 
     NSMutableArray *soundList = [NSMutableArray arrayWithCapacity:[soundElements count]];
-//    for (GDataXMLElement *soundElement in soundElements) {
-//        Sound *sound = [[Sound alloc] init];
-//        NSArray *soundChildElements = [soundElement children];
-//        if ([soundChildElements count] != 2) {
-//            [NSException raise:@"InvalidObjectException"
-//                        format:@"Sound must contain two child nodes"];
-//        }
-//        
-//        GDataXMLNode *nameChildNode = [soundChildElements firstObject];
-//        GDataXMLNode *fileNameChildNode = [soundChildElements lastObject];
-//        
-//        // swap values (if needed)
-//        if ([fileNameChildNode.name isEqualToString:@"name"] && [nameChildNode.name isEqualToString:@"fileName"]) {
-//            nameChildNode = fileNameChildNode;
-//            fileNameChildNode = nameChildNode;
-//        }
-//        
-//        if ((! [nameChildNode.name isEqualToString:@"name"]) || (! [fileNameChildNode.name isEqualToString:@"fileName"])) {
-//            [NSException raise:@"InvalidObjectException"
-//                        format:@"Sound must contains wrong child node(s)"];
-//        }
-//        sound.name = [nameChildNode stringValue];
-//        sound.fileName = [fileNameChildNode stringValue];
-//    }
+    SoundCBXMLNodeParser *soundParser = [SoundCBXMLNodeParser new];
+    for (GDataXMLElement *soundElement in soundElements) {
+        [soundList addObject:[soundParser parseFromElement:soundElement]];
+    }
     return soundList;
+}
+
+- (NSMutableArray*)parseAndCreateScripts:(GDataXMLElement*)objectElement
+{
+    NSArray *scriptListElements = [objectElement elementsForName:@"scriptList"];
+    [XMLError exceptionIf:[scriptListElements count] notEquals:1 message:@"No scriptList given!"];
+
+    NSArray *scriptElements = [[scriptListElements firstObject] children];
+    if (! [scriptElements count]) {
+        // TODO: ask team if we should return nil or an empty NSMutableArray in this case!!
+        return nil;
+    }
+
+    NSMutableArray *scriptList = [NSMutableArray arrayWithCapacity:[scriptElements count]];
+    ScriptCBXMLNodeParser *scriptParser = [ScriptCBXMLNodeParser new];
+    for (GDataXMLElement *scriptElement in scriptElements) {
+        [scriptList addObject:[scriptParser parseFromElement:scriptElement]];
+    }
+    return scriptList;
 }
 
 @end
