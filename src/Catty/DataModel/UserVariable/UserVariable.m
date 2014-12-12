8fb0e273d1b4d9e46a7045b896d44494e5a1852d
Ralph Samer <ralph.samer@student.tugraz.at>
2014-12-12 02:36:13 +0100

    implement user variable serialization

--- src/Catty/DataModel/UserVariable/UserVariable.m
+++ src/Catty/DataModel/UserVariable/UserVariable.m
@@ -32,25 +32,9 @@
     return [NSString stringWithFormat:@"UserVariable: Name: %@, Value: %@", self.name, self.value ];
 }
 
-- (GDataXMLElement*)toXMLforObject:(SpriteObject*)spriteObject
-{
-    GDataXMLElement *userVariableXMLElement = [GDataXMLNode elementWithName:@"userVariable"];
-    GDataXMLElement *nameXMLElement = [GDataXMLNode elementWithName:@"name" stringValue:self.name];
-    [userVariableXMLElement addChild:nameXMLElement];
-    return userVariableXMLElement;
-}
-
-- (GDataXMLElement*)toXMLforProgram:(Program*)program
-{
-    GDataXMLElement *userVariableXMLElement = [GDataXMLNode elementWithName:@"userVariable"];
-    [userVariableXMLElement addAttribute:[GDataXMLNode elementWithName:@"reference"
-                                                           stringValue:@"../../../../../objectList/object[10]/scriptList/startScript/brickList/setVariableBrick[2]/userVariable"]];
-    return userVariableXMLElement;
-}
-
 - (BOOL)isEqualToUserVariable:(UserVariable*)userVariable
 {
-    if([self.name isEqualToString:userVariable.name] && [Util isEqual:self.value toObject:userVariable.value])
+    if ([self.name isEqualToString:userVariable.name] && [Util isEqual:self.value toObject:userVariable.value])
         return YES;
     return NO;
 }
