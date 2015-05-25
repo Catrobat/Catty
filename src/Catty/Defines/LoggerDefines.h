2434fc32af203c0c46bf912d19232c82b8e44b43
Ralph Samer <ralph.samer@student.tugraz.at>
2015-05-26 00:13:27 +0200

    IOS-89 prepare for release/debug/test mode logging

---
+++ src/Catty/Defines/LoggerDefines.h
@@ -0,0 +1,60 @@
+/**
+ *  Copyright (C) 2010-2015 The Catrobat Team
+ *  (http://developer.catrobat.org/credits)
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  An additional term exception under section 7 of the GNU Affero
+ *  General Public License, version 3, is available at
+ *  (http://developer.catrobat.org/license_additional_term)
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see http://www.gnu.org/licenses/.
+ */
+
+// Logger names for release and debug mode configured in Swell.plist
+//------------------------------------------------------------------------------------------------------------
+#if DEBUG == 0
+//============================================================================================================
+//
+//                                             RELEASE MODE
+//
+//============================================================================================================
+#define kCBLoggerPlayerSchedulerID @"CBPlayerSchedulerLogger.Release"
+#define kCBLoggerPlayerFrontendID @"CBPlayerFrontendLogger.Release"
+#define kCBLoggerPlayerBackendID @"CBPlayerBackendLogger.Release"
+//------------------------------------------------------------------------------------------------------------
+
+#else // DEBUG == 1
+//============================================================================================================
+//
+//                                            DEVELOPER MODE
+//
+//============================================================================================================
+
+#define kCBLoggerPlayerSchedulerID @"CBPlayerSchedulerLogger.Debug"
+#define kCBLoggerPlayerFrontendID @"CBPlayerFrontendLogger.Debug"
+#define kCBLoggerPlayerBackendID @"CBPlayerBackendLogger.Debug"
+
+#endif // DEBUG
+
+
+
+//============================================================================================================
+//
+//                                            TEST MODE
+//
+//============================================================================================================
+
+// Test logger names configured in Swell.plist
+#define kCBTestLoggerPlayerSchedulerID @"CBPlayerSchedulerLogger.Test"
+#define kCBTestLoggerPlayerFrontendID @"CBPlayerFrontendLogger.Test"
+#define kCBTestLoggerPlayerBackendID @"CBPlayerBackendLogger.Test"
