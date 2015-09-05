a85b4926637f732d6eea8b1d8aa878b66afb3e6c
Ralph Samer <ralph.samer@student.tugraz.at>
2015-09-05 23:49:26 +0200

    first UITests for MyProgramsViewController + ProgramTableViewController

---
+++ src/Catty UITests/MyProgramsVCTests.swift
@@ -0,0 +1,56 @@
+/**
+ *  Copyright (C) 2010-2015 The Catrobat Team
+ *  (http://developer.catrobat.org/credits)
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  An additional term exception under section 7 of the GNU Affero
+ *  General Public License, version 3, is available at
+ *  (http://developer.catrobat.org/license_additional_term)
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see http://www.gnu.org/licenses/.
+ */
+
+import XCTest
+
+class MyProgramsVCTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+
+    func testCanCreateMyFirstProgramAfterAllProgramsHaveBeenDeleted() {
+        let app = XCUIApplication()
+        app.tables.staticTexts["Programs"].tap()
+        app.navigationBars["Programs"].buttons["Edit"].tap()
+        app.buttons["Delete Programs"].tap()
+        let toolbarsQuery = app.toolbars
+        toolbarsQuery.buttons["Select All"].tap()
+        toolbarsQuery.buttons["Delete"].tap()
+        XCTAssert(app.tables.staticTexts.count == 1)
+    }
+
+}
