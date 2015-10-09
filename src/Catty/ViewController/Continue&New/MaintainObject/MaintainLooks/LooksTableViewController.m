2474e40955d1ba88525f75b173ebfc51a6bd1ffb
Marc Slavec <marc.slavec@gmail.com>
2015-10-09 08:08:42 +0200

    fixes small warning

--- src/Catty/ViewController/Continue&New/MaintainObject/MaintainLooks/LooksTableViewController.m
+++ src/Catty/ViewController/Continue&New/MaintainObject/MaintainLooks/LooksTableViewController.m
@@ -658,12 +658,15 @@ static NSCharacterSet *blockedCharacterSet = nil;
         } else if (buttonIndex != actionSheet.cancelButtonIndex) {
             // draw new image
             NSDebug(@"Draw new image");
-            PaintViewController *vc = [self.storyboard instantiateViewControllerWithIdentifier:kPaintViewControllerIdentifier];
-            vc.delegate = self;
-            vc.editingPath = nil;
-            vc.programHeight = self.object.program.header.screenHeight.floatValue;
-            vc.programWidth = self.object.program.header.screenWidth.floatValue;
-            [self.navigationController pushViewController:vc animated:YES];
+            dispatch_async(dispatch_get_main_queue(), ^{
+                PaintViewController *vc = [self.storyboard instantiateViewControllerWithIdentifier:kPaintViewControllerIdentifier];
+                vc.delegate = self;
+                vc.editingPath = nil;
+                vc.programHeight = self.object.program.header.screenHeight.floatValue;
+                vc.programWidth = self.object.program.header.screenWidth.floatValue;
+                [self.navigationController pushViewController:vc animated:YES];
+            });
+
         } else {
             if (self.showAddLookActionSheetAtStartForObject || self.showAddLookActionSheetAtStartForScriptEditor) {
                 if (self.afterSafeBlock) {
