fec849b7cbd452bce98d0da53600d95024c18267
Marc Slavec <marc.slavec@gmail.com>
2015-10-15 10:03:38 +0200

    phi XML

--- src/Catty/Parser&Serialializer/New Parser & Serializer/XMLHandler/Bricks/Phiro/PhiroMotorStopBrick+CBXMLHandler.m
+++ src/Catty/Parser&Serialializer/New Parser & Serializer/XMLHandler/Bricks/Phiro/PhiroMotorStopBrick+CBXMLHandler.m
@@ -34,68 +34,34 @@
 
 @implementation PhiroMotorStopBrick (CBXMLHandler)
 
-//+ (instancetype)parseFromElement:(GDataXMLElement*)xmlElement withContextForLanguageVersion093:(CBXMLParserContext*)context
-//{
-//    NSUInteger childCount = [xmlElement.childrenWithoutComments count];
-//    GDataXMLElement *userVariableElement = nil;
-//    
-//    [CBXMLParserHelper validateXMLElement:xmlElement forFormulaListWithTotalNumberOfFormulas:1];
-//    
-//    if (childCount == 3) {
-//        userVariableElement = [xmlElement childWithElementName:@"userVariable"];
-//        [XMLError exceptionIfNil:userVariableElement message:@"No userVariableElement element found..."];
-//        
-//        GDataXMLElement *inUserBrickElement = [xmlElement childWithElementName:@"inUserBrick"];
-//        [XMLError exceptionIfNil:inUserBrickElement message:@"No inUserBrickElement element found..."];
-//        
-//        // inUserBrick code goes here...
-//    } else if (childCount == 2) {
-//        userVariableElement = [xmlElement childWithElementName:@"userVariable"];
-//        GDataXMLElement *inUserBrickElement = [xmlElement childWithElementName:@"inUserBrick"];
-//        
-//        if (userVariableElement == nil && inUserBrickElement == nil) {
-//            [XMLError exceptionWithMessage:@"Neither userVariableElement nor inUserBrickElement found..."];
-//        }
-//    } else if (childCount != 1) {
-//        [XMLError exceptionWithMessage:@"Too many or too less child elements..."];
-//    }
-//
-//    Formula *formula = [CBXMLParserHelper formulaInXMLElement:xmlElement forCategoryName:@"VARIABLE" withContext:context];
-//    PhiroMotorStopBrick *setVariableBrick = [self new];
-//    PhiroMotorStopBrick.variableFormula = formula;
-//    
-//    if (userVariableElement != nil) {
-//        UserVariable *userVariable = [context parseFromElement:userVariableElement withClass:[UserVariable class]];
-//        [XMLError exceptionIfNil:userVariable message:@"Unable to parse userVariable..."];
-//        
-//        setVariableBrick.userVariable = userVariable;
-//    }
-//    
-//    return setVariableBrick;
-//}
-//
-//+ (instancetype)parseFromElement:(GDataXMLElement*)xmlElement withContextForLanguageVersion095:(CBXMLParserContext*)context
-//{
-//    return [self parseFromElement:xmlElement withContextForLanguageVersion093:context];
-//}
-//
-//- (GDataXMLElement*)xmlElementWithContext:(CBXMLSerializerContext*)context
-//{
-//    NSUInteger indexOfBrick = [CBXMLSerializerHelper indexOfElement:self inArray:context.brickList];
-//    GDataXMLElement *brick = [GDataXMLElement elementWithName:@"brick" xPathIndex:(indexOfBrick+1) context:context];
-//    [brick addAttribute:[GDataXMLElement attributeWithName:@"type" escapedStringValue:@"SetVariableBrick"]];
-//    GDataXMLElement *formulaList = [GDataXMLElement elementWithName:@"formulaList" context:context];
-//    GDataXMLElement *formula = [self.variableFormula xmlElementWithContext:context];
-//    [formula addAttribute:[GDataXMLElement attributeWithName:@"category" escapedStringValue:@"VARIABLE"]];
++ (instancetype)parseFromElement:(GDataXMLElement*)xmlElement withContextForLanguageVersion093:(CBXMLParserContext*)context
+{
+    [CBXMLParserHelper validateXMLElement:xmlElement forNumberOfChildNodes:1 AndFormulaListWithTotalNumberOfFormulas:2];
+//    Formula *duration = [CBXMLParserHelper formulaInXMLElement:xmlElement forCategoryName:@"DURATION" withContext:context];
+    //    Formula *value = [CBXMLParserHelper formulaInXMLElement:xmlElement forCategoryName:@"VALUE" withContext:context];
+    PhiroMotorStopBrick *phiroMotorStopBrick = [self new];
+//    phiroMotorStopBrick.motor = motor;
+    //    phiroPlayToneBrick.tone = value;
+    return phiroMotorStopBrick;
+}
+
+
++ (instancetype)parseFromElement:(GDataXMLElement*)xmlElement withContextForLanguageVersion095:(CBXMLParserContext*)context
+{
+    return [self parseFromElement:xmlElement withContextForLanguageVersion093:context];
+}
+
+- (GDataXMLElement*)xmlElementWithContext:(CBXMLSerializerContext*)context
+{
+    NSUInteger indexOfBrick = [CBXMLSerializerHelper indexOfElement:self inArray:context.brickList];
+    GDataXMLElement *brick = [GDataXMLElement elementWithName:@"brick" xPathIndex:(indexOfBrick+1) context:context];
+    [brick addAttribute:[GDataXMLElement attributeWithName:@"type" escapedStringValue:@"PhiroMotorStopBrick"]];
+    GDataXMLElement *formulaList = [GDataXMLElement elementWithName:@"formulaList" context:context];
+//    GDataXMLElement *formula = [self.durationFormula xmlElementWithContext:context];
+//    [formula addAttribute:[GDataXMLElement attributeWithName:@"category" escapedStringValue:@"DURATION"]];
 //    [formulaList addChild:formula context:context];
-//    [brick addChild:formulaList context:context];
-//
-//    // add pseudo <inUserBrick> element to produce a Catroid equivalent XML (unused at the moment)
-//    [brick addChild:[GDataXMLElement elementWithName:@"inUserBrick" stringValue:@"false" context:context] context:context];
-//
-//    if (self.userVariable)
-//        [brick addChild:[self.userVariable xmlElementWithContext:context] context:context];
-//    return brick;
-//}
+    [brick addChild:formulaList context:context];
+    return brick;
+}
 
 @end
