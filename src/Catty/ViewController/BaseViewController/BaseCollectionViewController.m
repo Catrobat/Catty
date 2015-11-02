995c0d2ccafda2fdef98619d9f677d568e832111
Marc Slavec <marc.slavec@gmail.com>
2015-11-02 08:44:43 +0100

    introduced ResourceHelper

--- src/Catty/ViewController/BaseViewController/BaseCollectionViewController.m
+++ src/Catty/ViewController/BaseViewController/BaseCollectionViewController.m
@@ -32,12 +32,10 @@
 #import "LoadingView.h"
 #import "BDKNotifyHUD.h"
 #import "PlaceHolderView.h"
-#import "KeychainUserDefaultsDefines.h"
+#import "ResourceHelper.h"
 
 
 #import <CoreBluetooth/CoreBluetooth.h>
-#import "SensorHandler.h"
-#import <AudioToolbox/AudioToolbox.h>
 
 @class BluetoothPopupVC;
 
@@ -76,7 +74,7 @@
     self.scenePresenterViewController = [ScenePresenterViewController new];
     self.scenePresenterViewController.program = [Program programWithLoadingInfo:[Util lastUsedProgramLoadingInfo]];
     NSInteger resources = [self.scenePresenterViewController.program getRequiredResources];
-    if ([self checkResources:resources]) {
+    if ([ResourceHelper checkResources:resources delegate:self]) {
         [self startSceneWithVC:self.scenePresenterViewController];
     }
 }
@@ -85,118 +83,6 @@
 {
     [self.navigationController setToolbarHidden:YES animated:YES];
     [self.navigationController pushViewController:vc animated:YES];
-}
-
--(BOOL)checkResources:(NSInteger)requiredResources
-{
-    NSString *notAvailable = @"";
-    NSMutableArray *bluetoothArray = [NSMutableArray new];
-    if ((requiredResources & kTextToSpeech) > 0) {
-        //intern iOS AVSpeechSynthesizer always available
-    }
-    
-    if ((requiredResources & kFaceDetection) > 0) {
-        [[SensorHandler sharedSensorHandler] faceDetectionInit];
-    }
-    
-    if ((requiredResources & kVibration) > 0) {
-        NSInteger available = kSystemSoundID_Vibrate;
-        if (!available) {
-            if([notAvailable isEqualToString:@""]){
-                notAvailable = [NSString stringWithFormat:@"%@",kLocalizedVibration];
-            } else {
-                notAvailable = [NSString stringWithFormat:@"%@, %@",notAvailable,kLocalizedVibration];
-            }
-        }
-    }
-    if ((requiredResources & kLocation) > 0) {
-        if (![[SensorHandler sharedSensorHandler] locationAvailable]) {
-            if([notAvailable isEqualToString:@""]){
-                notAvailable = [NSString stringWithFormat:@"%@",kLocalizedSensorCompass];
-            } else {
-                notAvailable = [NSString stringWithFormat:@"%@, %@",notAvailable,kLocalizedSensorCompass];
-            }
-        }
-    }
-    if ((requiredResources & kAccelerometer) > 0) {
-        if (![[SensorHandler sharedSensorHandler] accelerometerAvailable]) {
-            if([notAvailable isEqualToString:@""]){
-                notAvailable = [NSString stringWithFormat:@"%@",kLocalizedSensorAcceleration];
-            } else {
-                notAvailable = [NSString stringWithFormat:@"%@, %@",notAvailable,kLocalizedSensorAcceleration];
-            }
-        }
-    }
-    if ((requiredResources & kGyro) > 0) {
-        if (![[SensorHandler sharedSensorHandler] gyroAvailable]) {
-            if([notAvailable isEqualToString:@""]){
-                notAvailable = [NSString stringWithFormat:@"%@",kLocalizedSensorRotation];
-            } else {
-                notAvailable = [NSString stringWithFormat:@"%@, %@",notAvailable,kLocalizedSensorRotation];
-            }
-        }
-    }
-    if ((requiredResources & kMagnetometer) > 0) {
-        if (![[SensorHandler sharedSensorHandler] magnetometerAvailable]) {
-            if([notAvailable isEqualToString:@""]){
-                notAvailable = [NSString stringWithFormat:@"%@",kLocalizedSensorMagnetic];
-            } else {
-                notAvailable = [NSString stringWithFormat:@"%@, %@",notAvailable,kLocalizedSensorMagnetic];
-            }
-        }
-    }
-    if ((requiredResources & kLoudness) > 0) {
-        if (![[SensorHandler sharedSensorHandler] loudnessAvailable]) {
-            if([notAvailable isEqualToString:@""]){
-                notAvailable = [NSString stringWithFormat:@"%@",kLocalizedSensorLoudness];
-            } else {
-                notAvailable = [NSString stringWithFormat:@"%@, %@",notAvailable,kLocalizedSensorLoudness];
-            }
-        }
-    }
-    if ((requiredResources & kLED) > 0) {
-        if (![[FlashHelper sharedFlashHandler] isAvailable]) {
-            if([notAvailable isEqualToString:@""]){
-                notAvailable = [NSString stringWithFormat:@"%@",kLocalizedSensorLED];
-            } else {
-                notAvailable = [NSString stringWithFormat:@"%@, %@",notAvailable,kLocalizedSensorLED];
-            }
-        }
-    }
-    if (![notAvailable isEqualToString:@""]) {
-        notAvailable = [NSString stringWithFormat:@"%@ %@",notAvailable,kLocalizedNotAvailable];
-        [Util confirmAlertWithTitle:kLocalizedPocketCode message:notAvailable delegate:self tag:kResourcesAlertView];
-        return NO;
-    }
-    //CheckBluetooth
-    
-    if ((requiredResources & kBluetoothPhiro) > 0 && kPhiroActivated) {
-        //ConnectPhiro
-        if (!([BluetoothService sharedInstance].phiro.state == CBPeripheralStateConnected)) {
-            [bluetoothArray addObject:[NSNumber numberWithInteger:BluetoothDeviceIDphiro]];
-        }
-    }
-    
-    if ((requiredResources & kBluetoothArduino) > 0 && kArduinoActivated) {
-        //ConnectArduino
-        if (!([BluetoothService sharedInstance].arduino.state == CBPeripheralStateConnected)) {
-            [bluetoothArray addObject:[NSNumber numberWithInteger:BluetoothDeviceIDarduino]];
-        }
-    }
-    if ( bluetoothArray.count > 0) {
-        
-        UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"iPhone" bundle: nil];
-        BluetoothPopupVC * bvc = (BluetoothPopupVC*)[storyboard instantiateViewControllerWithIdentifier:@"bluetoothPopupVC"];
-        [bvc setDeviceArray:bluetoothArray];
-        [bvc setDelegate:self];
-        [bvc setVc:self.scenePresenterViewController];
-        UINavigationController *navController = [[UINavigationController alloc] initWithRootViewController:(UIViewController*)bvc];
-        [self presentViewController:navController animated:YES completion:nil];
-        return NO;
-    } else {
-        return YES;
-    }
-    return YES;
 }
 
 #pragma mark - Setup Toolbar
