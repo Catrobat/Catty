54300bf12be5960689246d97ab00aa76c66e5e5c
Marc Slavec <marc.slavec@gmail.com>
2015-11-22 15:47:53 +0100

    small fix

--- src/Catty/PocketPaint/PaintViewController.m
+++ src/Catty/PocketPaint/PaintViewController.m
@@ -365,7 +365,7 @@
 - (void)changeAction
 {
     NSInteger height = self.view.frame.size.height * 0.7;
-    LCTableViewPickerControl *pickerView = [[LCTableViewPickerControl alloc] initWithFrame:CGRectMake(0, self.view.frame.size.height, self.view.frame.size.width, height) title:kLocalizedPaintPickItem value:self.activeAction items:self.actionTypeArray offset:CGPointMake(0, 0) navBarOffset:self.navigationController.navigationBar.frame.size.height+[UIApplication sharedApplication].statusBarFrame.size.height];
+    LCTableViewPickerControl *pickerView = [[LCTableViewPickerControl alloc] initWithFrame:CGRectMake(0, self.view.frame.size.height, self.view.frame.size.width, height) title:kLocalizedPaintPickItem value:self.activeAction items:self.actionTypeArray offset:CGPointMake(0, 100) navBarOffset:self.navigationController.navigationBar.frame.size.height+[UIApplication sharedApplication].statusBarFrame.size.height];
     pickerView.delegate = self;
     pickerView.height = height;
     self.navigationController.toolbarHidden = YES;
