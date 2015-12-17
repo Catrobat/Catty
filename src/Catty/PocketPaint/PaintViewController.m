03978e09d5012027ddceb9ca9d8032ec80a29e66
Marc Slavec <marc.slavec@gmail.com>
2015-12-17 09:30:53 +0100

    fixed bug when opening paint tools

--- src/Catty/PocketPaint/PaintViewController.m
+++ src/Catty/PocketPaint/PaintViewController.m
@@ -364,8 +364,8 @@
 
 - (void)changeAction
 {
-    NSInteger height = self.view.frame.size.height * 0.7;
-    LCTableViewPickerControl *pickerView = [[LCTableViewPickerControl alloc] initWithFrame:CGRectMake(0, self.view.frame.size.height, self.view.frame.size.width, height) title:kLocalizedPaintPickItem value:self.activeAction items:self.actionTypeArray offset:CGPointMake(0, 100) navBarOffset:self.navigationController.navigationBar.frame.size.height+[UIApplication sharedApplication].statusBarFrame.size.height];
+    NSInteger height = self.view.frame.size.height * 0.65;
+    LCTableViewPickerControl *pickerView = [[LCTableViewPickerControl alloc] initWithFrame:CGRectMake(0, self.view.frame.size.height, self.view.frame.size.width, height) title:kLocalizedPaintPickItem value:self.activeAction items:self.actionTypeArray screenHeight:self.view.frame.size.height+self.navigationController.navigationBar.frame.size.height+[UIApplication sharedApplication].statusBarFrame.size.height navBarOffset:self.navigationController.navigationBar.frame.size.height+[UIApplication sharedApplication].statusBarFrame.size.height];
     pickerView.delegate = self;
     pickerView.height = height;
     self.navigationController.toolbarHidden = YES;
