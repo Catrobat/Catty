5ae966ed9a5f46d35c69569fe9bf4a3b14e7d776
Pushkar Sharma <thepsguy@icloud.com>
2017-06-01 21:55:44 +0530

    > initialized IfThenLogicBeginBrick.m and IfThenLogicBeginBrick.h

--- src/Catty/DataModel/Bricks/Control/IfThenLogicBeginBrick.m
+++ src/Catty/DataModel/Bricks/Control/IfThenLogicBeginBrick.m
@@ -1,5 +1,5 @@
 /**
- *  Copyright (C) 2010-2018 The Catrobat Team
+ *  Copyright (C) 2010-2017 The Catrobat Team
  *  (http://developer.catrobat.org/credits)
  *
  *  This program is free software: you can redistribute it and/or modify
@@ -20,13 +20,12 @@
  *  along with this program.  If not, see http://www.gnu.org/licenses/.
  */
 
-#import "IfLogicBeginBrick.h"
-#import "IfLogicElseBrick.h"
+#import "IfThenLogicBeginBrick.h"
 #import "IfLogicEndBrick.h"
 #import "Util.h"
 #import "Script.h"
 
-@implementation IfLogicBeginBrick
+@implementation IfThenLogicBeginBrick
 
 - (BOOL)isAnimateable
 {
@@ -70,7 +69,7 @@
 
 - (NSString*)brickTitle
 {
-    return [kLocalizedIfBegin stringByAppendingString:[[@"%@ " stringByAppendingString:kLocalizedIfBeginSecondPart] stringByAppendingString:[[@" ... " stringByAppendingString:kLocalizedElse] stringByAppendingString:@" ..."]]];
+    return [kLocalizedIfBegin stringByAppendingString:[@"%@ " stringByAppendingString:kLocalizedIfBeginSecondPart]];
 }
 
 - (BOOL)checkCondition
@@ -92,11 +91,9 @@
 
 - (BOOL)isEqualToBrick:(Brick*)brick
 {
-    if(![Util isEqual:self.ifElseBrick.brickTitle toObject:((IfLogicBeginBrick*)brick).ifElseBrick.brickTitle])
+    if(![Util isEqual:self.ifEndBrick.brickTitle toObject:((IfThenLogicBeginBrick*)brick).ifEndBrick.brickTitle])
         return NO;
-    if(![Util isEqual:self.ifEndBrick.brickTitle toObject:((IfLogicBeginBrick*)brick).ifEndBrick.brickTitle])
-        return NO;
-    if(![self.ifCondition isEqualToFormula:((IfLogicBeginBrick*)brick).ifCondition])
+    if(![self.ifCondition isEqualToFormula:((IfThenLogicBeginBrick*)brick).ifCondition])
         return NO;
     return YES;
 }
