c3f948af4c67d9750af8baa05b1730aa4e31c4dc
Michael Herold <michael.herold@student.tugraz.at>
2017-08-23 10:28:43 +0200

    IOS-497 Changed instruction and position

--- src/Catty/PlayerEngine/Instructions/Motion/ChangeSizeByNBrick+Instruction.swift
+++ src/Catty/PlayerEngine/Instructions/Motion/ChangeSizeByNBrick+Instruction.swift
@@ -36,10 +36,6 @@ extension ChangeSizeByNBrick: CBInstructionProtocol{
             let sizeInPercent = size.interpretDoubleForSprite(object)
             spriteNode.xScale = CGFloat(spriteNode.xScale + CGFloat(sizeInPercent/100.0))
             spriteNode.yScale = CGFloat(spriteNode.yScale + CGFloat(sizeInPercent/100.0))
-            if let textBubble = spriteNode.childNodeWithName("textBubble")
-            {
-                textBubble.position = CGPoint(x: spriteNode.size.width/4, y: spriteNode.size.height/2)
-            }
         }
     }
 }
