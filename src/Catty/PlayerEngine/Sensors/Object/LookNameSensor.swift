04702fdf8cf92f974ea42793652fed4763c22213
Carmen Popa <carmen.popa@mready.net>
2018-07-24 23:06:53 +0300

    IOS-613 Change protocol for look name

--- src/Catty/PlayerEngine/Sensors/Object/LookNameSensor.swift
+++ src/Catty/PlayerEngine/Sensors/Object/LookNameSensor.swift
@@ -20,7 +20,7 @@
  *  along with this program.  If not, see http://www.gnu.org/licenses/.
  */
 
-@objc class LookNameSensor: NSObject, ObjectSensor {
+@objc class LookNameSensor: NSObject, ObjectStringSensor {
     
     static let tag = "OBJECT_LOOK_NAME"
     static let name = kUIFEObjectLookName
