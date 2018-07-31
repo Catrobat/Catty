adf38cc240752a085a299edda616f62a61f044a3
Carmen Popa <carmen.popa@mready.net>
2018-07-31 16:16:33 +0300

    IOS-622 Update with constraint for math table list

--- src/Catty/Functions/FunctionManager.swift
+++ src/Catty/Functions/FunctionManager.swift
@@ -53,7 +53,7 @@
             CeilFunction(),
             MaxFunction(),
             MinFunction(),
-            ElementFunction(),
+            //ElementFunction(),
             TrueFunction(),
             FalseFunction()
         ]
