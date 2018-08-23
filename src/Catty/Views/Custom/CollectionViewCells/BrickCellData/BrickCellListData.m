babecffc3b7e044943e8e88487a2b35ac2757542
Benjamin Wullschleger <benjamin.wullschleger@namics.com>
2018-08-23 10:53:04 +0200

    IOS-581 Do not show dialog to choose a variable/list for a brick, when there are no variables/lists available

--- src/Catty/Views/Custom/CollectionViewCells/BrickCellData/BrickCellListData.m
+++ src/Catty/Views/Custom/CollectionViewCells/BrickCellData/BrickCellListData.m
@@ -75,6 +75,10 @@
 - (void)comboboxOpened:(iOSCombobox *)combobox
 {
     [self.brickCell.dataDelegate disableUserInteractionAndHighlight:self.brickCell withMarginBottom:kiOSComboboxTotalHeight];
+    if (combobox.values.count == 1) {
+        [self comboboxDonePressed: combobox withValue:combobox.values.firstObject];
+    }
+
 }
 
 # pragma mark - User interaction
