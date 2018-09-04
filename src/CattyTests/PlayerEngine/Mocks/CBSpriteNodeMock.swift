9e7d816f951b01c14f81648452391b465437ae9f
Michael Herold <michael.herold@student.tugraz.at>
2018-09-04 08:25:27 +0200

    IOS-566 Move mock objects to different folder

--- src/CattyTests/PlayerEngine/Mocks/CBSpriteNodeMock.swift
+++ src/CattyTests/PlayerEngine/Mocks/CBSpriteNodeMock.swift
@@ -20,6 +20,8 @@
  *  along with this program.  If not, see http://www.gnu.org/licenses/.
  */
 
+@testable import Pocket_Code
+
 class CBSpriteNodeMock: CBSpriteNode {
     
     var mockedPosition : CGPoint?
