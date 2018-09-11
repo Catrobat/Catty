702093603c47b2d69afb8d8a31e77aa040c783f3
Carmen Popa <carmengpopa13@gmail.com>
2018-09-11 20:29:49 +0300

    IOS-629 Fix mod and element function (#1031)

--- src/CattyTests/PlayerEngine/Functions/List/ElementFunctionTest.swift
+++ src/CattyTests/PlayerEngine/Functions/List/ElementFunctionTest.swift
@@ -48,6 +48,14 @@ class ElementFunctionTest: XCTestCase {
         XCTAssertEqual(type(of: function).defaultValue as! String, function.value(firstParameter: 2 as AnyObject, secondParameter: userVariableNumber as AnyObject) as! String)
     }
     
+    func testEmptyList() {
+        let emptyList = UserVariable()
+        emptyList.value = []
+        
+        XCTAssertEqual(type(of: function).defaultValue as! String, function.value(firstParameter: 0 as AnyObject, secondParameter: emptyList as AnyObject) as! String)
+        XCTAssertEqual(type(of: function).defaultValue as! String, function.value(firstParameter: 1 as AnyObject, secondParameter: emptyList as AnyObject) as! String)
+    }
+    
     func testValue() {
         // number list
         let userVariableNumber = UserVariable()
