84481f426bbc939f30b67924a6446c3329be90e6
Michael Herold <michael.herold@student.tugraz.at>
2018-11-09 09:44:59 +0100

    IOS-533 Rename ArduinoAnalogPingFunctionTest to ArduinoAnalogPinFunctionTest

--- src/CattyTests/PlayerEngine/Functions/Arduino/ArduinoAnalogPinFunctionTest.swift
+++ src/CattyTests/PlayerEngine/Functions/Arduino/ArduinoAnalogPinFunctionTest.swift
@@ -24,7 +24,7 @@ import XCTest
 
 @testable import Pocket_Code
 
-class ArduinoAnalogPingFunctionTest: XCTestCase {
+class ArduinoAnalogPinFunctionTest: XCTestCase {
 
     var function: ArduinoAnalogPinFunction!
     var bluetoothService: BluetoothService!
