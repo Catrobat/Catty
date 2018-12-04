0c0efd787bf02c593418a585765cc801b116758b
Michael Herold <michael.herold@student.tugraz.at>
2018-12-04 08:36:30 +0100

    IOS-693 Use method from Util

--- src/Catty/ViewController/Scene/ScenePresenterViewController+Resources.swift
+++ src/Catty/ViewController/Scene/ScenePresenterViewController+Resources.swift
@@ -37,7 +37,7 @@ import CoreBluetooth
             let readyToStart = self.notifyUserAboutUnavailableResources(navigationController: navigationController)
 
             DispatchQueue.main.async {
-                if readyToStart && !(self.navigationController?.topViewController is ScenePresenterViewController) {
+                if readyToStart && !(Util.topmostViewController() is ScenePresenterViewController) {
                     navigationController.pushViewController(self, animated: true)
                 } else {
                     self.hideLoadingView()
