e544d8a41bab9d0e4319744c353abc9d14c1c484
Michael Herold <michael.herold@student.tugraz.at>
2018-12-18 09:24:42 +0100

    IOS-692 Repeat string in tests

--- src/Catty UITests/VariablesTests.swift
+++ src/Catty UITests/VariablesTests.swift
@@ -102,11 +102,7 @@ class VariablesTests: XCTestCase, UITestProtocol {
         XCTAssert(app.sheets["Variable type"].exists)
 
         app.buttons["for all objects"].tap()
-        app.alerts["New Variable"].textFields["Enter your variable name here..."].typeText("iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii"
-            + "iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii"
-            + "iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii"
-            + "iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii"
-            + "iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii")
+        app.alerts["New Variable"].textFields["Enter your variable name here..."].typeText(String(repeating: "i", count: 250))
         app.alerts["New Variable"].buttons["OK"].tap()
         XCTAssert(app.staticTexts["When program started"].exists)
     }
@@ -134,11 +130,7 @@ class VariablesTests: XCTestCase, UITestProtocol {
         XCTAssert(app.sheets["Variable type"].exists)
 
         app.buttons["for all objects"].tap()
-        app.alerts["New Variable"].textFields["Enter your variable name here..."].typeText("iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii"
-            + "iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii"
-            + "iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii"
-            + "iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii"
-            + "iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii")
+        app.alerts["New Variable"].textFields["Enter your variable name here..."].typeText(String(repeating: "i", count: 250 + 1))
         app.alerts["New Variable"].buttons["OK"].tap()
         XCTAssert(app.alerts["Pocket Code"].exists)
     }
