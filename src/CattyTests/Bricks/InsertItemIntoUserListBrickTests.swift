183217b011d3fb67ddfec79df1275a7e44aaa8c3
Michael Herold <michael.herold@student.tugraz.at>
2019-01-09 16:53:55 +0100

    IOS-730 Add test for InserItemIntoUserListBrick

---
+++ src/CattyTests/Bricks/InsertItemIntoUserListBrickTests.swift
@@ -0,0 +1,114 @@
+/**
+ *  Copyright (C) 2010-2019 The Catrobat Team
+ *  (http://developer.catrobat.org/credits)
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  An additional term exception under section 7 of the GNU Affero
+ *  General Public License, version 3, is available at
+ *  (http://developer.catrobat.org/license_additional_term)
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see http://www.gnu.org/licenses/.
+ */
+
+import XCTest
+
+@testable import Pocket_Code
+
+final class InsertItemIntoUserListBrickTests: XCTestCase {
+
+    var program: Program!
+    var spriteObject: SpriteObject!
+    var spriteNode: CBSpriteNode!
+    var script: Script!
+    var scheduler: CBScheduler!
+    var context: CBScriptContextProtocol!
+    var userList: UserVariable!
+    var brick: InsertItemIntoUserListBrick!
+
+    override func setUp() {
+        program = Program()
+        spriteObject = SpriteObject()
+        spriteObject.name = "SpriteObjectName"
+
+        spriteNode = CBSpriteNode(spriteObject: spriteObject)
+        spriteObject.spriteNode = spriteNode
+        spriteObject.program = program
+
+        script = Script()
+        script.object = spriteObject
+
+        spriteObject.program.variables = VariablesContainer()
+
+        userList = UserVariable()
+        userList.isList = true
+        spriteObject.program.variables.addObjectList(userList, for: spriteObject)
+
+        brick = InsertItemIntoUserListBrick()
+        brick.userList = userList
+        brick.script = script
+
+        let logger = CBLogger(name: "Logger")
+        let broadcastHandler = CBBroadcastHandler(logger: logger)
+        let formulaInterpreter = FormulaManager(sceneSize: Util.screenSize(true))
+        scheduler = CBScheduler(logger: logger, broadcastHandler: broadcastHandler, formulaInterpreter: formulaInterpreter)
+        context = CBScriptContext(script: script, spriteNode: spriteNode, formulaInterpreter: formulaInterpreter)
+    }
+
+    func testInserItem() {
+        XCTAssertNil(userList.value)
+
+        brick.index = Formula(integer: 1)
+        brick.elementFormula = Formula(integer: 1)
+
+        switch brick.instruction() {
+        case let .execClosure(closure):
+            closure(context, scheduler)
+        default:
+            XCTFail("Fatal Error")
+        }
+
+        XCTAssertNotNil(userList.value)
+    }
+
+    func testInserItemAtInvalidPosition() {
+        XCTAssertNil(userList.value)
+
+        brick.index = Formula(string: "abc")
+        brick.elementFormula = Formula(integer: 1)
+
+        switch brick.instruction() {
+        case let .execClosure(closure):
+            closure(context, scheduler)
+        default:
+            XCTFail("Fatal Error")
+        }
+
+        XCTAssertNil(userList.value)
+    }
+
+    func testInserItemAtNegativePosition() {
+        XCTAssertNil(userList.value)
+
+        brick.index = Formula(integer: -1)
+        brick.elementFormula = Formula(integer: 1)
+
+        switch brick.instruction() {
+        case let .execClosure(closure):
+            closure(context, scheduler)
+        default:
+            XCTFail("Fatal Error")
+        }
+
+        XCTAssertNil(userList.value)
+    }
+}
