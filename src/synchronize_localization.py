ffe5e29066f298692eb527d36143f3eaefc74235
Stefan Painhapp <stefan@painhapp.com>
2019-02-06 17:35:50 +0100

    CATTY-29 Enable Localization Defines in UI Tests

--- src/synchronize_localization.py
+++ src/synchronize_localization.py
@@ -14,7 +14,12 @@ newdata = newdata.replace("\", ", "\", comment: ")
 newdata = newdata.replace("nil", "\"\"")
 newdata = newdata.replace("\%", "%")
 
-
 f = open("Catty/Defines/LanguageTranslationDefinesSwift.swift",'w')
+f.write(newdata)
+f.close()
+
+newdata = newdata.replace("/* { PLACEHOLDER HEADER, DO NOT DELETE THIS LINE (used in synchronize_localization.py) } */", "import UIKit\n\nclass LanguageTranslation {}")
+newdata = newdata.replace("\", comment: ", "\", bundle: Bundle(for: LanguageTranslation.self), comment: ")
+f = open("Catty UITests/Defines/LanguageTranslationDefinesUI.swift",'w')
 f.write(newdata)
 f.close()
