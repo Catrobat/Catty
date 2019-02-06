8f24c67dfc41b2e159db4417405d9109d6fafd04
GatCode <gatcode@gmail.com>
2019-02-06 17:15:45 +0100

    IOS-718 Fix Test Cases

--- src/CattyTests/SpriteObjectMutableCopyTests.swift
+++ src/CattyTests/SpriteObjectMutableCopyTests.swift
@@ -138,23 +138,54 @@ final class SpriteObjectMutableCopyTests: XMLAbstractTest {
         }
     }
 
-    func testMutableCopyAndUpdateReference() {
-        let brickA = SetLookBrick()
-        brickA.look = Look(name: "brickA", andPath: "look")
+    func testMutableCopyAndUpdateReferenceForLook() {
+        let brick = SetLookBrick()
+        let lookA = Look(name: "lookA", andPath: "look")
+        let lookB = Look(name: "lookB", andPath: "look")
 
-        let brickB = SetLookBrick()
-        brickB.look = Look(name: "brickB", andPath: "look")
+        brick.look = lookA
 
         let context = CBMutableCopyContext()
-        context.updateReference(brickA, withReference: brickB)
+        context.updateReference(lookA, withReference: lookB)
         XCTAssertEqual(1, context.updatedReferences.count)
 
-        let brickCopy = brickA.mutableCopy(with: context) as! SetLookBrick
-        
-        // something isn't quite right here
-        XCTAssertNotEqual(brickA.look.name, brickCopy.look.name)
-        XCTAssertEqual(brickB.look.name, brickCopy.look.name)
-        XCTAssertFalse(brickA === brickCopy)
-        XCTAssertTrue(brickB === brickCopy)
+        let brickCopy = brick.mutableCopy(with: context) as! SetLookBrick
+
+        XCTAssertTrue(brickCopy.look === lookB)
+        XCTAssertFalse(brickCopy.look === lookA)
+    }
+
+    func testMutableCopyAndUpdateReferenceForBackground() {
+        let brick = SetBackgroundBrick()
+        let lookA = Look(name: "lookA", andPath: "look")
+        let lookB = Look(name: "lookB", andPath: "look")
+
+        brick.look = lookA
+
+        let context = CBMutableCopyContext()
+        context.updateReference(lookA, withReference: lookB)
+        XCTAssertEqual(1, context.updatedReferences.count)
+
+        let brickCopy = brick.mutableCopy(with: context) as! SetBackgroundBrick
+
+        XCTAssertTrue(brickCopy.look === lookB)
+        XCTAssertFalse(brickCopy.look === lookA)
+    }
+
+    func testMutableCopyAndUpdateReferenceForSound() {
+        let brick = PlaySoundBrick()
+        let soundA = Sound(name: "soundA", fileName: "soundA")
+        let soundB = Sound(name: "soundB", fileName: "soundB")
+
+        brick.sound = soundA
+
+        let context = CBMutableCopyContext()
+        context.updateReference(soundA, withReference: soundB)
+        XCTAssertEqual(1, context.updatedReferences.count)
+
+        let brickCopy = brick.mutableCopy(with: context) as! PlaySoundBrick
+
+        XCTAssertTrue(brickCopy.sound === soundB)
+        XCTAssertFalse(brickCopy.sound === soundA)
     }
 }
