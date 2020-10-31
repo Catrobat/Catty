7daf7f9ac13aebfe3e02149f9999df1f114f713c
neel-makhecha <neel.makhecha@gmail.com>
2020-10-31 18:16:52 +0530

    CATTY-358 Implement StoreAuthenticator

---
+++ src/Catty/Store/StoreAuthenticator.swift
@@ -0,0 +1,306 @@
+/**
+ *  Copyright (C) 2010-2020 The Catrobat Team
+ *  (http://developer.catrobat.org/credits)
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  An additional term exception under section 7 of the GNU Affero
+ *  General Public License, version 3, is available at
+ *  (http://developer.catrobat.org/license_additional_term)
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see http://www.gnu.org/licenses/.
+ */
+
+protocol StoreAuthenticatorProtocol {
+    func login(username: String, password: String, completion: @escaping (StoreAuthenticatorLoginError?) -> Void)
+    func register(username: String, password: String, email: String, completion: @escaping (StoreAuthenticatorRegisterError?) -> Void)
+    func logout()
+}
+
+enum StoreAuthenticatorRegisterError: Error {
+    case request(error: Error?, statusCode: Int)
+    case serverResponse(response: String)
+    case timeout
+    case unexpectedError
+}
+
+enum StoreAuthenticatorLoginError: Error {
+    case request(error: Error?, statusCode: Int)
+    case authenticationFailed
+    case userDoesNotExist
+    case timeout
+    case unexpectedError
+}
+
+final class StoreAuthenticator: StoreAuthenticatorProtocol {
+
+    let httpBoundary = "---------------------------98598263596598246508247098291---------------------------"
+    let usernameTag = "registrationUsername"
+    let passwordTag = "registrationPassword"
+    let emailTag = "registrationEmail"
+    let registrationCountryTag = "registrationCountry"
+    let defaultCountryCode = "US"
+    let statusCodeTag = "statusCode"
+    let statusCodeOK = "200"
+    let statusCodeRegistrationOK = "201"
+    let statusCodeAuthenticationFailed = "601"
+    let statusCodeUserDoesNotExist = "803"
+    let tokenTag = "token"
+    let answerTag = "answer"
+
+    let session: URLSession
+
+    var username: String?
+    var password: String?
+    var email: String?
+
+    init(session: URLSession = StoreAuthenticator.defaultSession()) {
+        self.session = session
+    }
+
+    private func setFormDataParameter(_ parameterID: String?, with data: Data?, forHTTPBody body: inout Data) {
+        if let anEncoding = "--\(httpBoundary)\r\n".data(using: .utf8) {
+            body.append(anEncoding)
+        }
+        let parameterString = "Content-Disposition: form-data; name=\"\(parameterID ?? "")\"\r\n\r\n"
+        if let anEncoding = parameterString.data(using: .utf8) {
+            body.append(anEncoding)
+        }
+        if let aData = data {
+            body.append(aData)
+        }
+        if let anEncoding = "\r\n".data(using: .utf8) {
+            body.append(anEncoding)
+        }
+    }
+
+    private func handleLoginResponse(data: Data, response: HTTPURLResponse) -> StoreAuthenticatorLoginError? {
+
+        var dictionary: [String: Any]?
+        do {
+            dictionary = try JSONSerialization.jsonObject(with: data) as? [String: Any]
+        } catch {
+            NSLog(error.localizedDescription)
+            return .unexpectedError
+        }
+
+        guard let statusCode = dictionary?[statusCodeTag] else {
+            return .unexpectedError
+        }
+
+        debugPrint("StatusCode is: \(statusCode)")
+
+        if statusCodeOK == "\(statusCode)" {
+            debugPrint("Login successful")
+            guard let token = dictionary?[tokenTag] else {
+                return .unexpectedError
+            }
+            debugPrint("Token is: \(token)")
+
+            UserDefaults.standard.set(true, forKey: NetworkDefines.kUserIsLoggedIn)
+            UserDefaults.standard.set(self.username!, forKey: kcUsername)
+
+            JNKeychain.saveValue(token, forKey: NetworkDefines.kUserLoginToken)
+
+            return nil
+        } else if statusCodeAuthenticationFailed == "\(statusCode)" {
+            return .authenticationFailed
+        } else if statusCodeUserDoesNotExist == "\(statusCode)"{
+            return .userDoesNotExist
+        } else {
+            return .timeout
+        }
+
+    }
+
+    private func handleRegisterResponse(data: Data, response: URLResponse) -> StoreAuthenticatorRegisterError? {
+
+        var dictionary: [String: Any]?
+        do {
+            dictionary = try JSONSerialization.jsonObject(with: data) as? [String: Any]
+        } catch {
+            NSLog(error.localizedDescription)
+            return .unexpectedError
+        }
+
+        guard let statusCode = dictionary?[statusCodeTag] else {
+            return .unexpectedError
+        }
+        debugPrint("Statuscode is \(statusCode)")
+
+        if statusCodeOK == "\(statusCode)" || statusCodeRegistrationOK == "\(statusCode)" {
+            debugPrint("Registration successful")
+
+            guard let token = dictionary?[tokenTag] else {
+                return .unexpectedError
+            }
+
+            debugPrint("Token is \(token)")
+
+            UserDefaults.standard.set(true, forKey: NetworkDefines.kUserIsLoggedIn)
+            UserDefaults.standard.set(self.username, forKey: kcUsername)
+            UserDefaults.standard.set(self.email, forKey: kcEmail)
+
+            JNKeychain.saveValue(token, forKey: NetworkDefines.kUserLoginToken)
+
+            return nil
+        } else {
+            guard let serverResponseString = dictionary?[answerTag] else {
+                return .unexpectedError
+            }
+
+            return .serverResponse(response: "\(serverResponseString)")
+        }
+
+    }
+
+    func login(username: String, password: String, completion: @escaping (StoreAuthenticatorLoginError?) -> Void) {
+
+        debugPrint("Login started with username: \(username) and password: \(password)")
+        self.username = username
+        self.password = password
+
+        guard let url = URL.init(string: NetworkDefines.loginUrl) else {
+            return
+        }
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+
+        let contentType = "multipart/form-data; boundary=\(httpBoundary)"
+        request.addValue(contentType, forHTTPHeaderField: "Content-Type")
+
+        var body = Data()
+        self.setFormDataParameter(usernameTag, with: username.data(using: .utf8), forHTTPBody: &body)
+        self.setFormDataParameter(passwordTag, with: password.data(using: .utf8), forHTTPBody: &body)
+
+        let encoding = "--\(httpBoundary)--\r\n"
+
+        if let data = encoding.data(using: .utf8) {
+            body.append(data)
+        }
+
+        request.httpBody = body
+
+        let postLength = String(format: "%lu", UInt(body.count))
+        request.addValue(postLength, forHTTPHeaderField: "Content-Length")
+
+        request.timeoutInterval = TimeInterval(NetworkDefines.connectionTimeout)
+
+        print("REQUEST #1")
+        print(request.allHTTPHeaderFields)
+
+        let task = self.session.dataTask(with: request) { data, response, error in
+
+            guard let response = response as? HTTPURLResponse else {
+                completion(.unexpectedError)
+                return
+            }
+
+            if let error = error as NSError?, error.domain == NSURLErrorDomain && error.code == NSURLErrorTimedOut {
+                completion(.timeout)
+            }
+
+            guard let data = data, response.statusCode == 200, error == nil else {
+                if response.statusCode == 401 {
+                    completion(.authenticationFailed)
+                    return
+                }
+                completion(.request(error: error, statusCode: response.statusCode))
+                return
+            }
+
+            print("RESPONSE #1")
+            print(response)
+            completion(self.handleLoginResponse(data: data, response: response))
+
+        }
+
+        task.resume()
+    }
+
+    func register(username: String, password: String, email: String, completion: @escaping (StoreAuthenticatorRegisterError?) -> Void) {
+
+        debugPrint("Register started with username: \(username) and password: \(password) and email: \(email)")
+        self.username = username
+        self.password = password
+        self.email = email
+
+        guard let url = URL.init(string: NetworkDefines.registerUrl) else {
+            completion(.unexpectedError)
+            return
+        }
+
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+
+        let contentType = "multipart/form-data; boundary=\(httpBoundary)"
+        request.addValue(contentType, forHTTPHeaderField: "Content-Type")
+
+        var body = Data()
+        self.setFormDataParameter(usernameTag, with: username.data(using: .utf8), forHTTPBody: &body)
+        self.setFormDataParameter(passwordTag, with: password.data(using: .utf8), forHTTPBody: &body)
+        self.setFormDataParameter(emailTag, with: email.data(using: .utf8), forHTTPBody: &body)
+
+        let countryCode = Locale.current.regionCode ?? defaultCountryCode
+        debugPrint("Current Country is: \(countryCode)")
+
+        self.setFormDataParameter(registrationCountryTag, with: countryCode.data(using: .utf8), forHTTPBody: &body)
+
+        let encoding = "--\(httpBoundary)--\r\n"
+
+        if let data = encoding.data(using: .utf8) {
+            body.append(data)
+        }
+
+        request.httpBody = body
+        request.timeoutInterval = TimeInterval(NetworkDefines.connectionTimeout)
+
+        let postLength = String(format: "%lu", UInt(body.count))
+        request.addValue(postLength, forHTTPHeaderField: "Content-Length")
+
+        let task = self.session.dataTask(with: request) { data, response, error in
+
+            guard let response = response as? HTTPURLResponse else {
+                completion(.unexpectedError)
+                return
+            }
+
+            if let error = error as NSError?, error.domain == NSURLErrorDomain && error.code == NSURLErrorTimedOut {
+                completion(.timeout)
+            }
+
+            guard let data = data, response.statusCode == 200, error == nil else {
+                completion(.request(error: error, statusCode: response.statusCode))
+                return
+            }
+
+            completion(self.handleRegisterResponse(data: data, response: response))
+
+        }
+
+        task.resume()
+
+    }
+
+    func logout() {
+        UserDefaults.standard.setValue(false, forKey: NetworkDefines.kUserIsLoggedIn)
+        UserDefaults.standard.setValue("", forKey: NetworkDefines.kUserLoginToken)
+        UserDefaults.standard.setValue("", forKey: kcUsername)
+    }
+
+    static func defaultSession() -> URLSession {
+        let config = URLSessionConfiguration.ephemeral
+        config.timeoutIntervalForRequest = Double(NetworkDefines.connectionTimeout)
+        return URLSession(configuration: config, delegate: nil, delegateQueue: nil)
+    }
+
+}
---
+++ src/CattyTests/Store/Authenticate/StoreAuthenticatorTests.swift
@@ -0,0 +1,231 @@
+/**
+ *  Copyright (C) 2010-2020 The Catrobat Team
+ *  (http://developer.catrobat.org/credits)
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  An additional term exception under section 7 of the GNU Affero
+ *  General Public License, version 3, is available at
+ *  (http://developer.catrobat.org/license_additional_term)
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see http://www.gnu.org/licenses/.
+ */
+
+import DVR
+@testable import Pocket_Code
+import XCTest
+
+class StoreAuthenticatorTests: XCTestCase {
+
+    func testLoginSuccess() {
+        let dvrSession = Session(cassetteName: "StoreAuthenticator.login.success.authentication")
+
+        let expectation = XCTestExpectation(description: "Login")
+
+        let authenticator = StoreAuthenticator(session: dvrSession)
+        authenticator.login(username: "test_user", password: "test_user") { error in
+            if error != nil {
+                XCTFail("There was an error while loggin in!")
+            }
+
+            expectation.fulfill()
+        }
+
+        wait(for: [expectation], timeout: 5)
+    }
+
+    func testLoginAuthenticationFailed() {
+        let dvrSession = Session(cassetteName: "StoreAuthenticator.login.fail.authentication")
+
+        let expectation = XCTestExpectation(description: "Login")
+
+        let authenticator = StoreAuthenticator(session: dvrSession)
+        authenticator.login(username: "test_user", password: "incorrect_password") { error in
+            guard let error = error else {
+                XCTFail("authentication was succesful. Replace username and password with some other values.")
+                return
+            }
+            XCTAssertEqual(error, .authenticationFailed)
+
+            expectation.fulfill()
+        }
+
+        wait(for: [expectation], timeout: 5)
+    }
+
+    func testLoginUserDoesNotExistError() {
+        let dvrSession = Session(cassetteName: "StoreAuthenticator.login.fail.userdoesnotexist")
+
+        let expectation = XCTestExpectation(description: "Login")
+
+        let authenticator = StoreAuthenticator(session: dvrSession)
+        authenticator.login(username: "random_user", password: "test_user") { error in
+            guard let error = error else {
+                XCTFail("authentication was succesful. Replace username and password with some other values.")
+                return
+            }
+            XCTAssertEqual(error, .userDoesNotExist)
+
+            expectation.fulfill()
+        }
+
+        wait(for: [expectation], timeout: 5)
+    }
+
+    func testRegisterSuccess() {
+        let dvrSession = Session(cassetteName: "StoreAuthenticator.register.success.authentication")
+
+        let expectation = XCTestExpectation(description: "Register")
+
+        let authenticator = StoreAuthenticator(session: dvrSession)
+        authenticator.register(username: "new_test_user", password: "new_test_user_password", email: "new_test_user@email.com") { error in
+            if error != nil {
+                XCTFail("An error occured")
+                return
+            }
+
+            expectation.fulfill()
+        }
+
+        wait(for: [expectation], timeout: 5)
+    }
+
+    func testRegisterServerResponseEmailAlreadyExist() {
+        let dvrSession = Session(cassetteName: "StoreAuthenticator.register.fail.emailalreadyinuse")
+
+        let expectation = XCTestExpectation(description: "Register")
+
+        let authenticator = StoreAuthenticator(session: dvrSession)
+        authenticator.register(username: "test_user2", password: "new_test_user_password", email: "test_user@email.com") { error in
+
+            guard let error = error else {
+                XCTFail("No error occured")
+                return
+            }
+
+            switch error {
+            case let .serverResponse(response: response):
+                XCTAssertTrue(response == "This email address already exists.")
+
+            default:
+                XCTFail("wrong error received")
+            }
+
+            expectation.fulfill()
+        }
+
+        wait(for: [expectation], timeout: 5)
+    }
+
+    func testRegisterServerResponseUsernameAlreadyExist() {
+        let dvrSession = Session(cassetteName: "StoreAuthenticator.register.fail.usernamealreadyexist")
+
+        let expectation = XCTestExpectation(description: "Register")
+
+        let authenticator = StoreAuthenticator(session: dvrSession)
+        authenticator.register(username: "test_user", password: "new_test_user_password", email: "test_user2@email.com") { error in
+
+            guard let error = error else {
+                XCTFail("No error occured")
+                return
+            }
+
+            switch error {
+            case let .serverResponse(response: response):
+                XCTAssertTrue(response == "This username already exists.")
+
+            default:
+                XCTFail("wrong error received")
+            }
+
+            expectation.fulfill()
+        }
+
+        wait(for: [expectation], timeout: 5)
+    }
+
+    func testRegisterServerResponseInvalidEmailID() {
+        let dvrSession = Session(cassetteName: "StoreAuthenticator.register.fail.invalidemail")
+
+        let expectation = XCTestExpectation(description: "Register")
+
+        let authenticator = StoreAuthenticator(session: dvrSession)
+        authenticator.register(username: "test_user", password: "new_test_user_password", email: "test_user@email") { error in
+
+            guard let error = error else {
+                XCTFail("No error occured")
+                return
+            }
+
+            switch error {
+            case let .serverResponse(response: response):
+                XCTAssertTrue(response == "Your email seems to be invalid")
+
+            default:
+                XCTFail("wrong error received")
+            }
+
+            expectation.fulfill()
+        }
+
+        wait(for: [expectation], timeout: 5)
+    }
+
+    func testRegisterServerResponseInvalidField() {
+        let dvrSession = Session(cassetteName: "StoreAuthenticator.register.fail.invalidfield")
+
+        let expectation = XCTestExpectation(description: "Register")
+
+        let authenticator = StoreAuthenticator(session: dvrSession)
+        authenticator.register(username: "@test_user*", password: "new_test_user_password", email: "test_user3@email.com") { error in
+
+            guard let error = error else {
+                XCTFail("No error occured")
+                return
+            }
+
+            switch error {
+            case let .serverResponse(response: response):
+                XCTAssertTrue(response == "This value is not valid.")
+
+            default:
+                XCTFail("wrong error received")
+            }
+
+            expectation.fulfill()
+        }
+
+        wait(for: [expectation], timeout: 5)
+    }
+}
+
+extension StoreAuthenticatorLoginError: Equatable {
+    public static func == (lhs: StoreAuthenticatorLoginError, rhs: StoreAuthenticatorLoginError) -> Bool {
+        switch (lhs, rhs) {
+        case (.request, .request), (.authenticationFailed, .authenticationFailed), (.unexpectedError, .unexpectedError), (.userDoesNotExist, .userDoesNotExist), (.timeout, .timeout):
+            return true
+        default:
+            return false
+        }
+    }
+}
+
+extension StoreAuthenticatorRegisterError: Equatable {
+    public static func == (lhs: StoreAuthenticatorRegisterError, rhs: StoreAuthenticatorRegisterError) -> Bool {
+        switch (lhs, rhs) {
+        case (.request, .request), (.serverResponse, .serverResponse), (.unexpectedError, .unexpectedError), (.timeout, .timeout):
+            return true
+        default:
+            return false
+        }
+    }
+}
