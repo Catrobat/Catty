3a2f775ad10472d44d354993f3d1199b6bc3d015
Amela Kurtic <amela.kurtic@student.tugraz.at>
2021-02-13 22:32:35 +0100

    CATTY-513 Add missing tests for Formula display string

--- src/CattyTests/Formula/FormulaTest.swift
+++ src/CattyTests/Formula/FormulaTest.swift
@@ -208,4 +208,59 @@ final class FormulaTest: XCTestCase {
 
         XCTAssertFalse(copiedFormula.formulaTree.rightChild.value.isEqual(formula.formulaTree.rightChild.value))
     }
+
+    func testGetDisplayString() {
+
+        let formulaString = "1.0"
+        var formula = Formula(string: formulaString)!
+        let formattedString = String(format: "'%@'", formulaString)
+        var displayString = formula.getDisplayString()
+        XCTAssertEqual(displayString, formattedString)
+
+        formula = Formula(double: 1.0)
+        displayString = formula.getDisplayString()
+        XCTAssertEqual(displayString, "1.00")
+
+        formula = Formula(float: 1.0)
+        displayString = formula.getDisplayString()
+        XCTAssertEqual(displayString, "1.00")
+
+        formula = Formula(integer: 1)
+        displayString = formula.getDisplayString()
+        XCTAssertEqual(displayString, "1")
+
+        let internTokenList = NSMutableArray()
+        let token1 = InternToken(type: TOKEN_TYPE_FUNCTION_NAME, andValue: "ROUND")!
+        let token2 = InternToken(type: TOKEN_TYPE_FUNCTION_PARAMETERS_BRACKET_OPEN)!
+        let token3 = InternToken(type: TOKEN_TYPE_NUMBER, andValue: "1.1111")!
+        let token4 = InternToken(type: TOKEN_TYPE_FUNCTION_PARAMETERS_BRACKET_CLOSE)!
+        internTokenList.add(token1)
+        internTokenList.add(token2)
+        internTokenList.add(token3)
+        internTokenList.add(token4)
+
+        var tokenList = [InternToken]()
+        for tokenElement in internTokenList {
+            if let internToken = tokenElement as? InternToken {
+                tokenList.append(internToken)
+            }
+        }
+
+        let internParser = InternFormulaParser(tokens: tokenList, andFormulaManager: formulaManager)!
+        let parseTree = internParser.parseFormula(for: nil)
+        XCTAssertNotNil(parseTree)
+
+        formula = Formula(formulaElement: parseTree)!
+        displayString = formula.getDisplayString()
+        XCTAssertEqual(displayString, "round( 1.1111 )")
+    }
+
+    func testSetDisplayStringManually() {
+        let formula = Formula()
+        let formulaString = "1.0"
+        let formattedString = String(format: "'%@'", formulaString)
+        formula.displayString = formattedString as NSString
+        let displayString = formula.getDisplayString()
+        XCTAssertEqual(displayString, formattedString)
+    }
 }
