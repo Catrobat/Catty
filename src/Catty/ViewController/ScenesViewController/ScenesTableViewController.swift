7d7f4ccd7d20617a4739ebc81088ffeecb2c7bf1
Luca Topolovec <topolovec@student.tugraz.at>
2023-10-22 20:01:48 +0200

    Implicit return for ScenesTableVC

--- src/Catty/ViewController/ScenesViewController/ScenesTableViewController.swift
+++ src/Catty/ViewController/ScenesViewController/ScenesTableViewController.swift
@@ -51,9 +51,9 @@ class ScenesTableViewController: UITableViewController, AddNewSceneDelegate {
         newScene = true
     }
 
-    override func numberOfSections(in tableView: UITableView) -> Int {return 1}
+    override func numberOfSections(in tableView: UITableView) -> Int { 1 }
 
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {return project.scenes.count}
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int { project.scenes.count }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 
