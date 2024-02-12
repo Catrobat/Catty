2263e33aa8da65dc0ba3a90e9a239a5fa84a6372
mt1622 <maria.theussl@student.tugraz.at>
2024-02-12 03:44:03 +0100

    CATTY-142
    added alert within projectsviewcontroller -> default objects are missing

--- src/Catty/ViewController/Projects/MyProjectsViewController.h
+++ src/Catty/ViewController/Projects/MyProjectsViewController.h
@@ -24,6 +24,7 @@
 #import "BaseTableViewController.h"
 #import "Project.h"
 
+
 @interface MyProjectsViewController : BaseTableViewController
 @property (nonatomic, strong) Project *selectedProject;
 @end
